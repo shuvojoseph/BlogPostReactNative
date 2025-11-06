d65cb8ec8bbc5824cd4977b1f7572fd62f70f570
shuvojoseph <shuvo.ewu.cse@gmail.com>
2025-11-06 16:17:17 +0600

    #Added different API_BASE_URL for Android, Blog list is showing on Android as well.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 
 const Config = {
-  API_BASE_URL: 'http://localhost:8080', // change this per environment
+
+  API_BASE_URL: 'http://localhost:8080', // change this per environment //iOS : http://localhost:8080 Android : http://10.0.2.2:8080
   // We can add staging/prod variants here
 };
 
