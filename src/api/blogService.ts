710f92d403ac931af8656062b5a3904123f743d9
shuvojoseph <shuvo.ewu.cse@gmail.com>
2025-11-06 15:40:43 +0600

    #Updated url for Blogs, getAllBlogs api is working fine & showing the list of Blogs now.

--- src/api/blogService.ts
+++ src/api/blogService.ts
@@ -3,22 +3,22 @@ import { BlogPayload } from '../models/blog';
 
 const blogService = {
   getBlogs: async () => {
-    const res = await apiClient.get('/api/Blogs');
+    const res = await apiClient.get('/api/blogs');
     return res.data; // expect array
   },
 
   addBlog: async (payload: BlogPayload) => {
-    const res = await apiClient.post('/api/Blogs', payload);
+    const res = await apiClient.post('/api/blogs', payload);
     return res.data;
   },
 
   updateBlog: async (id: string, payload: BlogPayload) => {
-    const res = await apiClient.put(`/api/Blogs/${id}`, payload);
+    const res = await apiClient.put(`/api/blogs/${id}`, payload);
     return res.data;
   },
 
   deleteBlog: async (id: string) => {
-    const res = await apiClient.delete(`/api/Blogs/${id}`);
+    const res = await apiClient.delete(`/api/blogs/${id}`);
     return res.data;
   },
 
