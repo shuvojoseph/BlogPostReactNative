a9a60f9bc221b1a774048f6181f80c9e3b1cf8ee
shuvojoseph <shuvo.ewu.cse@gmail.com>
2025-11-11 13:35:29 +0600

    #Added Render URL.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 
 const Config = {
 
-  API_BASE_URL: 'http://localhost:8080', // change this per environment //iOS : http://localhost:8080 Android : http://10.0.2.2:8080
+  API_BASE_URL: 'https://blogpostspringboot.onrender.com', // change this per environment //iOS : http://localhost:8080 Android : http://10.0.2.2:8080
   // We can add staging/prod variants here
 };
 
